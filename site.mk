36e29bae89c953017e5b670f1e470149ac52c955
Marcel <MTRNord@users.noreply.github.com>
2017-03-29 16:41:39 +0200

    Revert wrong changes

--- site.mk
+++ site.mk
@@ -20,7 +20,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-web-network \
 	gluon-web-wifi-config \
 	gluon-web-private-wifi \
-        gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
@@ -31,11 +30,11 @@ GLUON_SITE_PACKAGES := \
 # from ffki-packages:
 GLUON_SITE_PACKAGES += \
 	gluon-config-mode-contact-info-anonymous-hint
-
+	
 # from ffslfl-packages:
 GLUON_SITE_PACKAGES += \
-#	fff-random \
-#	ffslfl-fastd \
+	fff-random \
+	ffslfl-fastd \
 	ffslfl-tunneldigger \
 	ffslfl-vpn-select \
 	tunneldigger
